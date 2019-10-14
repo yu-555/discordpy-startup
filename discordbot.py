9df73a71faf9609ba420d81b0721784ca84a1f87
yu-555 <46838765+yu-555@users.noreply.github.com>
2019-10-14 20:32:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,12 @@
 from discord.ext import commands
 import os
 import traceback
+import discord
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -17,10 +20,10 @@ async def ping(ctx):
 async def nekoo(ctx):
     await ctx.send('にゃおーん')
     
-@bot.command()
+@client.event
 async def on_message(message):
-    if bot.user in message.mentions: # 話しかけられたかの判定
-        reply = f'{message.author.mention} 呼んだ？ + /n + いや読んでないか' # 返信メッセージの作成
+    if client.user in message.mentions: # 話しかけられたかの判定
+        reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
         await message.channel.send(reply) # 返信メッセージを送信
 
 bot.run(token)
