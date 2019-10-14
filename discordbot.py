10bdc1d3106301572cb2304c259a30435fecebaf
yu-555 <46838765+yu-555@users.noreply.github.com>
2019-10-14 19:24:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,21 +10,7 @@ client = discord.Client()
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
-@bot.event
-async def on_message(message):
-    if bot.user in message.mentions: # 話しかけられたかの判定
-        reply = f'{message.author.mention} 呼んだ？ + /n + いや読んでないか' # 返信メッセージの作成
-        await message.channel.send(reply) # 返信メッセージを送信
-        
-@bot.command()
-async def join(message):
-    if message=='/join':
-        role = discord.utils.get(message.guild.roles, name='test')
-        await message.author.add_roles(role)
-        reply = f'{message.author.mention} ようこそ！'
-        await message.channel.send(reply)
-        
-        
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
