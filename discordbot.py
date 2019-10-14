8317f01bb978c49ce47cc5bebdf953d0078e159e
yu-555 <46838765+yu-555@users.noreply.github.com>
2019-10-14 17:28:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,5 +19,12 @@ async def ping(ctx):
 async def nekoo(ctx):
     await ctx.send('にゃおーん')
 
+@client.event
+async def on_message(message):
+    if client.user in message.mentions: # 話しかけられたかの判定
+        reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
+        await message.channel.send(reply)
+    
+    
 
 bot.run(token)
