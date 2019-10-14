d212be2def87746fa8583b18de1c4a3468b728da
yu-555 <46838765+yu-555@users.noreply.github.com>
2019-10-14 18:24:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,6 +10,11 @@ client = discord.Client()
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
+@bot.event
+async def on_message(message):
+    if bot.user in message.mentions: # 話しかけられたかの判定
+        reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
+        await message.channel.send(reply) # 返信メッセージを送信
 
 @bot.command()
 async def ping(ctx):
